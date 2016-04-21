9349d13f7d67802c8cb1357d29e6bb76465de02d
Ilia Garaga <inform@rem-prim.ru>
2016-04-21 15:15:41 +1000

    move time captions to timetemplate class

--- app/Http/Controllers/TerminalController.php
+++ app/Http/Controllers/TerminalController.php
@@ -214,65 +214,17 @@ class TerminalController extends Controller
 
         $room = Room::find($room_id);
         
-        $timetemplate = $room->timetemplate->name;
+        $timetemplate = $room->timetemplate;
 
-        $times = $this->{"getTimeCaption_" . $timetemplate}($busyTimes);
+        $times = $timetemplate->getTimeCaption($busyTimes);
 
         return response()->json([
             'day' => $request->day,
             'dialog' => view('terminals.time', [
-                'type' => $timetemplate,
+                'type' => $timetemplate->name,
                 'times' => $times,
             ])->render()
         ]);
-    }
-
-    private function getTimeCaption_8_17_half_hour($busyTimes)
-    {
-        $result = [];
-
-        $date1 = strtotime("8 hour midnight");
-        $date2 = strtotime("17 hour midnight");
-        $add = 30 * 60; // полчаса
-
-        for ($date = $date1; $date < $date2; $date += $add) {
-            if (12 == date("H", $date)) {
-                continue;
-            }
-            $time = date("H:i", $date);
-            $disabled = 'false';
-            if (isset($busyTimes[$time])) {
-                $disabled = 'true';
-            }
-
-            $result[] =  ['caption' => $time, 'disabled' => $disabled];
-        }
-
-        return $result;
-    }
-
-    private function getTimeCaption_8_17_third_hour($busyTimes)
-    {
-        $result = [];
-
-        $date1 = strtotime("8 hour midnight");
-        $date2 = strtotime("17 hour midnight");
-        $add = 20 * 60; // треть часа
-
-        for ($date = $date1; $date < $date2; $date += $add) {
-            if (12 == date("H", $date)) {
-                continue;
-            }
-            $time = date("H:i", $date);
-            $disabled = 'false';
-            if (isset($busyTimes[$time])) {
-                $disabled = 'true';
-            }
-
-            $result[] =  ['caption' => $time, 'disabled' => $disabled];
-        }
-
-        return $result;
     }
 
 }
--- app/Timetemplate.php
+++ app/Timetemplate.php
@@ -17,4 +17,60 @@ class Timetemplate extends Model
     {
         return $this->belongsTo('suo\Room');
     }
+
+    public function getTimeCaption($busyTimes)
+    {
+        $method = 'getTimeCaption_' . $this->name;
+
+        return $this->$method($busyTimes);
+    }
+
+    private function getTimeCaption_8_17_half_hour($busyTimes)
+    {
+        $result = [];
+
+        $date1 = strtotime("8 hour midnight");
+        $date2 = strtotime("17 hour midnight");
+        $add = 30 * 60; // полчаса
+
+        for ($date = $date1; $date < $date2; $date += $add) {
+            if (12 == date("H", $date)) {
+                continue;
+            }
+            $time = date("H:i", $date);
+            $disabled = 'false';
+            if (isset($busyTimes[$time])) {
+                $disabled = 'true';
+            }
+
+            $result[] =  ['caption' => $time, 'disabled' => $disabled];
+        }
+
+        return $result;
+    }
+
+    private function getTimeCaption_8_17_third_hour($busyTimes)
+    {
+        $result = [];
+
+        $date1 = strtotime("8 hour midnight");
+        $date2 = strtotime("17 hour midnight");
+        $add = 20 * 60; // треть часа
+
+        for ($date = $date1; $date < $date2; $date += $add) {
+            if (12 == date("H", $date)) {
+                continue;
+            }
+            $time = date("H:i", $date);
+            $disabled = 'false';
+            if (isset($busyTimes[$time])) {
+                $disabled = 'true';
+            }
+
+            $result[] =  ['caption' => $time, 'disabled' => $disabled];
+        }
+
+        return $result;
+    }
+
 }
